614661a14e5684d3e6ee25fd6f6ef76715762e0f
nickyinluo <nickyinluo@tencent.com>
2023-09-26 20:10:16 +0800

    adjust 1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-variable "vpc_cidr_block"{
-    default = "10.2.0.0/16"
+variable "vpc_cidr_block" {
+  default = "10.2.0.0/16"
 }
 
 variable "availability_zone" {
@@ -11,7 +11,7 @@ resource "tencentcloud_vpc" "main" {
   cidr_block = var.vpc_cidr_block
 }
 
-resource "tencentcloud_subnet" "main" {
+resource "tencentcloud_subnet" "main" { #
   name              = "my_subnet"
   availability_zone = var.availability_zone
   vpc_id            = tencentcloud_vpc.main.id
